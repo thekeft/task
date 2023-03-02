2037d77a5ea998bcb25e65e647a1e8c491880d7b
thekeft <thekeft@daum.net>
2023-03-02 10:00:40 +0900

    코드 정리

--- src/main/java/task/htmldivider/feign/UrlFeignClient.java
+++ src/main/java/task/htmldivider/feign/UrlFeignClient.java
@@ -1,20 +1,14 @@
 package task.htmldivider.feign;
 
 import feign.Headers;
-import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.net.URI;
 
-@FeignClient(name = "url-client", url = "http://placeholder-url", configuration = Config.class)
+@FeignClient(name = "url-client", url = "http://placeholder", configuration = Config.class)
 public interface UrlFeignClient {
 
-//    @RequestLine("GET")
-//    @Headers("Content-Type: text/html")
-//    String getHtml(URI uri);
-
     @GetMapping()
-    @Headers("Accept: text/html")
     String getHtml(URI baseUri);
 }
